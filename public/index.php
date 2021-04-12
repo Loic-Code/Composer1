d01a75326edb79d1078bf8899b3f8e653a6315d0
Loic-Code <loic.pinguet.code@gmail.com>
2021-04-12 10:36:27 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+
+<?php
+
+require_once "../vendor/autoload.php";
+use Wcs\Wcs\Hello;
+
+
+$hello = new Hello();
+
+echo $hello->say();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wcs\Wcs;
+
+class Hello
+{
+    public function say(): string
+    {
+        return 'Hello World';
+    }
+}
